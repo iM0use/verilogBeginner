f99dd3b12837e0670a4471ba9b17cbef4f8919ce
hall <84882703+iM0use@users.noreply.github.com>
2022-03-29 11:16:20 +0800

    Create vector0.v

---
+++ vectors/vector0.v
@@ -0,0 +1,47 @@
+// Build a circuit that has one 3-bit input, then outputs the same vector, and also splits it into three separate 1-bit outputs. Connect output o0 to the input vector's position 0, o1 to position 1, etc.
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); // Module body starts after module declaration
+    assign o0 = vec[0];
+    assign o1 = vec[1];
+    assign o2 = vec[2];
+    assign outv = vec;
+endmodule
+
+// office sol
+/*
+module top_module(
+	input [2:0] vec, 
+	output [2:0] outv,
+	output o2,
+	output o1,
+	output o0
+);
+	
+	assign outv = vec;
+
+	// This is ok too: assign {o2, o1, o0} = vec;
+	assign o0 = vec[0];
+	assign o1 = vec[1];
+	assign o2 = vec[2];
+	
+endmodule
+*/
+
+/*
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); // Module body starts after module declaration
+    assign {o2, o1, o0} = outv;
+    //assign o0 = vec[0];
+    //assign o1 = vec[1];
+    //assign o2 = vec[2];
+    assign outv = vec;
+endmodule
+/*

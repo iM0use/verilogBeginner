8967146e6772352d8b4075c748550778e63e55c3
hall <84882703+iM0use@users.noreply.github.com>
2022-03-29 09:23:13 +0800

    Create baby.v

---
+++ BabyRuner/baby.v
@@ -0,0 +1,7 @@
+// Build a circuit with no inputs and one output that outputs a constant 0
+module top_module (
+    output zero
+    // not assigning a value to a signal usually results in 0
+);
+  
+endmodule

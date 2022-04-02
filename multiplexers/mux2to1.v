9a2d8d55fc5203ff17361bca8f744b8cc895d206
hall <84882703+iM0use@users.noreply.github.com>
2022-04-02 11:34:38 +0800

    Create mux2to1.v

---
+++ multiplexers/mux2to1.v
@@ -0,0 +1,24 @@
+// Create a one-bit wide, 2-to-1 multiplexer. When sel=0, choose a. When sel=1, choose b. 
+module top_module( 
+    input a, b, sel,
+    output out ); 
+    assign out = (sel)?b:a;
+endmodule
+
+// office sol
+/*
+module top_module (
+	input a,
+	input b,
+	input sel,
+	output out
+);
+
+	assign out = (sel & b) | (~sel & a);	// Mux expressed as AND and OR
+	
+	// Ternary operator is easier to read, especially if vectors are used:
+	// assign out = sel ? b : a;
+	
+endmodule
+
+*/
